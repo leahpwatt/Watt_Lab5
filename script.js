b3b561af857499b0f208f0fc718f8ac220a319f3
Leah <leahpwatt@gmail.com>
2016-04-15 16:47:19 -0400

    updates with celsius conversion

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-var temp = 32
-var celsius = (temp-32) * 5/9
-console.log(celsius)
+// This is what I did.
+
+var temp = 100;
+var celsius = (temp-32) * 5/9;
+console.log(celsius);
+
+
+var temp = 0;
+var fahrenheit = (temp * 1.8) + 32;
+console.log(fahrenheit);
+
+
+// This is what James taught us.
+
+var temp = 0;
+var convertedTemp = (temp-32) 8 5/9;
+console.log(convertedTemp);
+
+
+var temp = 0;
+var unit = "F";
+var convertedTemp;
+
+if(unit === F){
+	convertedTemp = (temp-32) 8 5/9;
+}	else {
+	convertedTemp = (temp * 9/5 - 32)
+}
+
+(temp-32) 8 5/9; // F to C
+
+(temp * 9/5 - 32) // C to F 
+
+console.log(convertedTemp);
